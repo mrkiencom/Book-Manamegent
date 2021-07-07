d5f753f670ebe04e22ef4346758f19fffe1dbecd
mrkiencom <kien3031999@gmail.com>
2021-07-07 17:19:25 +0700

    update

--- src/author/author.entity.ts
+++ src/author/author.entity.ts
@@ -1,3 +1,4 @@
+import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -5,5 +6,6 @@ export class Author {
   @PrimaryGeneratedColumn('uuid')
   id: string;
   @Column()
+  @IsNotEmpty()
   name: string;
 }
