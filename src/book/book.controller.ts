541d03f46560d0a9085f829124258e8c08d524e5
mrkiencom <kien3031999@gmail.com>
2021-07-08 15:16:36 +0700

    book-api

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Book } from './book.entity';
+import { BookService } from './book.service';
+import { BookDto } from './dto/book-dto';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  constructor(private bookService: BookService) {}
+  @Get()
+  getAllBook(): Promise<Book[]> {
+    return this.bookService.getAllBook();
+  }
+  @Get('/:id')
+  getBookById(@Param('id') id: string): Promise<Book> {
+    return this.bookService.getBookById(id);
+  }
+  @Post()
+  createBook(@Body() newBook: BookDto): Promise<Book> {
+    return this.bookService.createBook(newBook);
+  }
+  @Patch('/:id')
+  updateBook(
+    @Param('id') id: string,
+    @Body() newInfoBook: BookDto,
+  ): Promise<Book> {
+    return this.bookService.updateBook(id, newInfoBook);
+  }
+  @Delete('/:id')
+  deleteBook(@Param('id') id: string): Promise<string> {
+    return this.bookService.deleteBook(id);
+  }
+}
